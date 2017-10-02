df18dfd6da510220efddf7e216b4a35d6226659b
Brian Lee <brian.lee@servicemax.com>
2017-10-02 16:46:23 -0700

    Added react-router and react-router-dom; added routing implementation

--- src/App.js
+++ src/App.js
@@ -5,16 +5,27 @@ import SearchField from 'containers/SearchField'
 import Layout from 'components/Layout'
 import Sidebar from 'components/Sidebar'
 
+// Switch and Router from react-router for routing
+import { Switch, Route } from 'react-router'
+
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
           <Sidebar />
-          <div>
-            <SearchField placeholder="Search..." />
-            <Blog />
-          </div>
+          <Switch>
+            <Route
+              exact
+              path="/"
+              render={() => (
+                <div>
+                  <SearchField placeholder="Search..." />
+                  <Blog />
+                </div>
+              )}
+            />
+          </Switch>
         </Layout>
       </div>
     )
--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import store from './store'
 import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker'
 
+// BrowserRouter imports
+import { BrowserRouter } from 'react-router-dom'
+
 ReactDOM.render(
   //Provide store throughout the app
   <Provider store={store}>
-    <App />
+    <BrowserRouter basename="/">
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 )
