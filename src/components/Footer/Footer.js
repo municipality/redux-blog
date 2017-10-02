1746f7df2aea25f8b60a6d8bc0658d2bb8f9fe9b
Brian Lee <brian.lee@servicemax.com>
2017-10-02 16:32:05 -0700

    Added footer component; fixed some Avatar css

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => (
+  <div className="footer">
+    <ul>
+      <li>To do 1</li>
+      <li>To do 2</li>
+    </ul>
+  </div>
+)
+
+export default Footer
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Avatar from 'components/Avatar'
+import Footer from 'components/Footer'
 import './Sidebar.css'
 
 const image = 'https://i.ytimg.com/vi/7HKVvcNOQb0/maxresdefault.jpg'
 const Sidebar = () => (
   <div className="sidebar">
     <Avatar img={image} />
+    <Footer />
   </div>
 )
 
