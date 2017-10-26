6823b4f2bf067289939ca3ba8f6cede697c43c04
Brian Lee <brian.lee@ge.com>
2017-10-26 14:02:31 -0700

    Styled Entry, EntryList; Added media.scss for easier mobile styling

--- server.js
+++ server.js
@@ -29,15 +29,23 @@ router.get('/retrieveentries', function(req, res) {
     res.json([
       {
         title: 'Test title 1',
-        text: 'Test text 1',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Consequat nisl vel pretium lectus quam. Tempus urna et pharetra pharetra. Nunc non blandit massa enim nec dui nunc. Commodo odio aenean sed adipiscing. Ipsum dolor sit amet consectetur. Placerat duis ultricies lacus sed turpis tincidunt id. Euismod lacinia at quis risus sed vulputate odio ut enim. Tincidunt arcu non sodales neque sodales ut. Feugiat vivamus at augue eget arcu dictum. Ornare aenean euismod elementum nisi quis eleifend. Laoreet non curabitur gravida arcu ac tortor dignissim convallis. Consequat nisl vel pretium lectus quam. Rhoncus aenean vel elit scelerisque mauris.
+
+Nisi porta lorem mollis aliquam ut. Lorem mollis aliquam ut porttitor leo a diam sollicitudin. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Ultricies mi eget mauris pharetra et ultrices. Mattis rhoncus urna neque viverra justo nec ultrices dui. Aliquet lectus proin nibh nisl condimentum id venenatis. Accumsan tortor posuere ac ut consequat semper viverra nam. Velit euismod in pellentesque massa placerat duis ultricies. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Eget mi proin sed libero enim. Eget magna fermentum iaculis eu non diam phasellus. Enim lobortis scelerisque fermentum dui. Purus semper eget duis at. Consequat ac felis donec et odio.
+
+`,
         isOpen: true,
         tags: ['tag1', 'tag2'],
-        date: new Date(),
+        date: Date.now(),
         img: 'banner.jpg'
       },
       {
         title: 'Test title 2',
-        text: 'Test text 2',
+        text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Malesuada fames ac turpis egestas integer eget aliquet. Consequat nisl vel pretium lectus quam. Tempus urna et pharetra pharetra. Nunc non blandit massa enim nec dui nunc. Commodo odio aenean sed adipiscing. Ipsum dolor sit amet consectetur. Placerat duis ultricies lacus sed turpis tincidunt id. Euismod lacinia at quis risus sed vulputate odio ut enim. Tincidunt arcu non sodales neque sodales ut. Feugiat vivamus at augue eget arcu dictum. Ornare aenean euismod elementum nisi quis eleifend. Laoreet non curabitur gravida arcu ac tortor dignissim convallis. Consequat nisl vel pretium lectus quam. Rhoncus aenean vel elit scelerisque mauris.
+
+Nisi porta lorem mollis aliquam ut. Lorem mollis aliquam ut porttitor leo a diam sollicitudin. Mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Ultricies mi eget mauris pharetra et ultrices. Mattis rhoncus urna neque viverra justo nec ultrices dui. Aliquet lectus proin nibh nisl condimentum id venenatis. Accumsan tortor posuere ac ut consequat semper viverra nam. Velit euismod in pellentesque massa placerat duis ultricies. Euismod nisi porta lorem mollis aliquam ut porttitor leo a. Eget mi proin sed libero enim. Eget magna fermentum iaculis eu non diam phasellus. Enim lobortis scelerisque fermentum dui. Purus semper eget duis at. Consequat ac felis donec et odio.
+
+`,
         isOpen: true,
         tags: ['tag1', 'tag2', 'tag3', 'tag4'],
         date: new Date()
--- src/components/Entry/Entry.js
+++ src/components/Entry/Entry.js
@@ -3,21 +3,44 @@ import PropTypes from 'prop-types'
 import Tag from 'containers/Tag'
 import './Entry.css'
 
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+]
 //List of props used in Entry component
-const Entry = ({ title, text, img, tags, isOpen }) => (
-  <div className="entry">
-    <p>{title}</p>
-    {isOpen && (
-      <div>
-        {img && <img className="entry-image" alt="brianslaundry" src={img} />}
-        <div className="entry-text">{text}</div>
-        <div className="entry-tags">
-          {tags.map((tag, index) => <Tag key={tag} text={tag} />)}
-        </div>
+const Entry = ({ title, text, img, tags, isOpen, date }) => {
+  const dateObj = new Date(date)
+  const month = months[dateObj.getMonth()]
+
+  let dateString = `${dateObj.getDate()} ${month} ${dateObj.getFullYear()}`
+  return (
+    <div className="entry">
+      <div className="entry-header">
+        <div className="entry-title">{title.toUpperCase()}</div>
+        <div className="entry-date">{dateString}</div>
       </div>
-    )}
-  </div>
-)
+      {isOpen && (
+        <div>
+          {img && <img className="entry-image" alt="brianslaundry" src={img} />}
+          <div className="entry-text">{text}</div>
+          <div className="entry-tags">
+            {tags.map((tag, index) => <Tag key={tag} text={tag} />)}
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
 
 Entry.propTypes = {
   title: PropTypes.string.isRequired,
